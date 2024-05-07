e181bbc5467f1017fc0efce0baa2f1a7311b0c79
Ben Ford <benford_266@hotmail.com>
2024-05-07 13:47:31 +0100

    Add files via upload

---
+++ RoundDisplayScreen.ino
@@ -0,0 +1,148 @@
+
+#include <LovyanGFX.hpp>
+#include <esp_now.h>
+#include <WiFi.h>
+
+//ESP Board MAC Address:  68:67:25:6A:51:70
+class LGFX : public lgfx::LGFX_Device
+{
+
+  lgfx::Panel_GC9A01 _panel_instance;
+
+  lgfx::Bus_SPI _bus_instance;
+
+public:
+  LGFX(void)
+  {
+    {
+      auto cfg = _bus_instance.config();
+
+      
+      cfg.spi_host = SPI2_HOST;
+      cfg.spi_mode = 0;                  
+      cfg.freq_write = 80000000;         
+      cfg.freq_read = 20000000;          
+      cfg.spi_3wire = true;              
+      cfg.use_lock = true;               
+      cfg.dma_channel = SPI_DMA_CH_AUTO; 
+      cfg.pin_sclk = 6;  
+      cfg.pin_mosi = 7;  
+      cfg.pin_miso = -1; 
+      cfg.pin_dc = 2;    
+      _bus_instance.config(cfg);              
+      _panel_instance.setBus(&_bus_instance); 
+    }
+
+    {                                     
+      auto cfg = _panel_instance.config(); 
+
+      cfg.pin_cs = 10;  
+      cfg.pin_rst = -1;  
+      cfg.pin_busy = -1; 
+      cfg.memory_width = 240;   
+      cfg.memory_height = 240;  
+      cfg.panel_width = 240;   
+      cfg.panel_height = 240;   
+      cfg.offset_x = 0;         
+      cfg.offset_y = 0;         
+      cfg.offset_rotation = 0;  
+      cfg.dummy_read_pixel = 8; 
+      cfg.dummy_read_bits = 1; 
+      cfg.readable = false;     
+      cfg.invert = true;        
+      cfg.rgb_order = false;    
+      cfg.dlen_16bit = false;   
+      cfg.bus_shared = false;   
+
+      _panel_instance.config(cfg);
+    }
+
+    setPanel(&_panel_instance); 
+  }
+};
+
+
+LGFX display;
+
+typedef struct test_struct {
+  int WMIPressure =999;
+} test_struct;
+
+//Create a struct_message called myData
+test_struct myData;
+
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("WMI Pressure: ");
+  Serial.println(myData.WMIPressure);
+  Serial.println();
+}
+
+void setup(void)
+{
+  // SPIConfig
+  display.init();
+  display.initDMA();
+  display.startWrite();
+  display.setColor(0, 0, 0);
+  //Backlight config
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH);
+  display.setTextSize(3);
+  display.drawString("BFGarage",50,110);
+  delay(5000);
+  display.clear();
+
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+
+}
+uint32_t count = ~0;
+void loop(void)
+{
+  if (myData.WMIPressure == 999){
+  // Wait to recieve data from sensor
+  display.startWrite();
+  display.setTextSize(1.5);
+  display.drawString("By BFGarage",60,150);
+  display.setTextSize(2);
+  display.drawString("Connecting.",50,110);
+  delay(1000);
+  display.drawString("Connecting..",50,110);
+  delay(1000);
+  display.drawString("Connecting...",50,110);
+  delay(1000);
+  display.clear();
+  display.endWrite();
+  }
+  else
+  {
+    // Update screen with pressure data
+    display.setTextSize(7);
+    display.drawString(String(myData.WMIPressure),60,90);
+    display.setTextSize(4);
+    display.drawString("PSI",80,170);
+    delay(1000);
+    display.clear();
+    
+  }
+
+  
+
+}
---
+++ RoundDisplaySender.ino
@@ -0,0 +1,70 @@
+#include <esp_now.h>
+#include <WiFi.h>
+// ESP Board MAC Address:  8C:AA:B5:94:E3:94
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0x68, 0x67, 0x25, 0x6A, 0x51, 0x70};
+typedef struct test_struct {
+  int x;
+} test_struct;
+
+test_struct test;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// Sensor config
+float inputP1;
+int outputP1;
+int SensorMaxPSI = 300; // Max pressure sensor can read
+int SensorMinRes = 416; // 0.5v value of 12 bit ADC read
+int SensorMaxRes = 3750; // 4.5 value of 12 bit ADC read
+int SensorRangeRes = SensorMaxRes - SensorMinRes; // Range of values between 0.5v and 4.5v 
+
+
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.mode(WIFI_STA);
+ 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+
+  // Read value from ADC
+  inputP1 = analogRead(A12); //12 Bit 
+  // Maths to ADC vaule into PSI Unit
+  outputP1 = ((inputP1 - SensorMinRes)* SensorMaxPSI /SensorRangeRes);
+  // Put sensor data into var
+  test.x = outputP1;
+  // Send sensor data over ESP NOW
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
+  // Data send delay
+  delay(1000);
+}
