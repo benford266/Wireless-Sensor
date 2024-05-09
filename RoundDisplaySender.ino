99d9148e90c36e9f97d82b627eb841bf9707b4ac
Ben Ford <benford_266@hotmail.com>
2024-05-09 14:39:04 +0100

    Update RoundDisplaySender.ino

--- RoundDisplaySender.ino
+++ RoundDisplaySender.ino
@@ -3,8 +3,10 @@
 // ESP Board MAC Address:  8C:AA:B5:94:E3:94
 // REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
 uint8_t broadcastAddress1[] = {0x68, 0x67, 0x25, 0x6A, 0x51, 0x70};
+uint8_t broadcastAddress2[] = {0x68, 0x67, 0x25, 0x6A, 0x51, 0x70};
 typedef struct test_struct {
-  int x;
+  int sensor1;
+  int sensor2;
 } test_struct;
 
 test_struct test;
@@ -25,8 +27,12 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 
 // Sensor config
 float inputP1;
+float inputP2;
 int outputP1;
-int SensorMaxPSI = 300; // Max pressure sensor can read
+int sensor1PSI;
+int sensor2PSI;
+int Sensor1MaxPSI = 300; // Max pressure sensor can read
+int Sensor2MaxPSI = 300; // Max pressure sensor can read
 int SensorMinRes = 416; // 0.5v value of 12 bit ADC read
 int SensorMaxRes = 3750; // 4.5 value of 12 bit ADC read
 int SensorRangeRes = SensorMaxRes - SensorMinRes; // Range of values between 0.5v and 4.5v 
@@ -53,16 +59,24 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+}
 }
  
 void loop() {
 
   // Read value from ADC
   inputP1 = analogRead(A12); //12 Bit 
+  inputP2 = analogRead(A13); //12 Bit 
   // Maths to ADC vaule into PSI Unit
-  outputP1 = ((inputP1 - SensorMinRes)* SensorMaxPSI /SensorRangeRes);
+  sensor1PSI = ((inputP1 - SensorMinRes)* Sensor1MaxPSI /SensorRangeRes);
+  sensor2PSI = ((inputP2 - SensorMinRes)* Sensor2MaxPSI /SensorRangeRes);
   // Put sensor data into var
-  test.x = outputP1;
+  test.sensor1 = sensor1PSI;
+  test.sensor2 = sensor2PSI;
   // Send sensor data over ESP NOW
   esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
   // Data send delay
